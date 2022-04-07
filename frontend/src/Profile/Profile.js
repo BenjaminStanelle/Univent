4f6cc0df2083911a32ac9666428f74b6d74396a1
Benjamin Stanelle <benji.stanelle@hotmail.com>
2022-04-06 20:43:38 -0500

    Update Profile.js

--- frontend/src/Profile/Profile.js
+++ frontend/src/Profile/Profile.js
@@ -18,28 +18,37 @@ import Image from "react-bootstrap/Image";
 // import profile_pic from "../images/club_symbol.png";
 
 const Profile = () => {
+  /*use state is a hook that allows you to have state variables in functional components.
+  Allows us to register state which then is managed inside of a component, when state is changed, 
+  the component re-renders*/
   const [loadedUser, setLoadedUser] = useState();
   const [loadedEmail, setLoadedEmail] = useState();
-  // const [loadedStudentID, setLoadedStudentID] = useState();
   const [loadedImage, setLoadedImage] = useState();
 
+  //
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  //gets dynamic user id front the URL.
   const userId = useParams().userId;
 
+  //use affect runs for one render, thereafter runs if the dependencies sendRequest, or userID are changed.
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const responseData = await sendRequest(
-          `http://localhost:5000/api/users/account/${userId}`
+        //await returns a promise object, waiting until the promise accepts or rejects.
+        const responseData = await sendRequest( 
+          `http://localhost:5000/api/users/account/${userId}` /*Sends get request(by default), 
+                                                              to backend with a dynamic user id.*/
+                                                              //send request function fron http-hook.js
         );
-        setLoadedUser(responseData.existingUser.name);
-        setLoadedEmail(responseData.existingUser.email);
+        //Loading response data into states
+        setLoadedUser(responseData.existingUser.name); 
+        setLoadedEmail(responseData.existingUser.email); //loading into loadedEmail
         setLoadedImage(responseData.existingUser.image);
-        //setLoadedStudentID(responseData.existingUser.studentID);
+
       } catch (err) {}
     };
-    fetchUser();
-  }, [sendRequest, userId]);
+    fetchUser();  
+  }, [sendRequest, userId]);  //dependencies of useEffect
   console.log(loadedImage);
 
   return (
