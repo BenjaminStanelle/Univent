4b80be7263c0af84e5027a679f160d0ab51ce18b
Akshar Patel <aksharpatel0710@gmail.com>
2022-04-18 11:27:34 -0500

    events list added

---
+++ frontend/src/events/EventsList.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Card, Button, Row, Col } from "react-bootstrap";
+
+const EventsList = (props) => {
+  // POST method implementation for creating new events:
+
+  return (
+    <Row>
+      <Col md={12}>
+        {props.EVENTS.map((ev) => (
+          <Card key={ev.id} style={{ margin: "1%", height: "10rem" }}>
+            <Card.Header
+              style={{
+                fontWeight: "500",
+                fontSize: 20,
+                fontFamily: "Copperplate",
+              }}
+            >
+              {ev.title.replace("_", " ")}
+            </Card.Header>
+            <Card.Body style={{ margin: "0%" }}>
+              <Row>
+                <Col md={2}>
+                  <Card.Img
+                    src={ev.image}
+                    style={{
+                      height: "5.5rem",
+                      width: "5.5rem",
+                      borderRadius: "50%",
+                      margin: "auto",
+                    }}
+                  />
+                </Col>
+                <Col md={8}>
+                  <div style={{ textAlign: "left", paddingTop: "2%" }}></div>
+                  <p>{ev.date}</p>
+                  <p>{ev.address}</p>
+                </Col>
+                <Col md={2}>
+                  <Button variant="primary" className="mt-4">
+                    More Detail
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        ))}
+      </Col>
+    </Row>
+  );
+};
+
+export default EventsList;
